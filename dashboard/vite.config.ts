cd7e1ab7870b62296e5ed2113c177bc706fcf745
Sofiyan-Shaikh <183908419+Sofiyan-Shaikh@users.noreply.github.com>
2025-12-06 18:48:37 +0530

    Add Render deploy config

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // increase limit from 500 → 2000 KB
+  }
 })
